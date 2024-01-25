5f317f9579a02a374065cadb4d6aef44d1c076dd
saikrishna-malepati <sai192sai195@gmail.com>
2024-01-25 13:32:39 +0530

    intro to middleswares

--- TASK 6/app.js
+++ TASK 6/app.js
@@ -1,14 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express =require('express');
-const app =express();
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
-app.use((req,res,next)=>{
-    console.log('in the another middleware');
-    res.send('<h1>hello my dear friend</h1>');
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
