4340f6f840897e3e651d0130980bc0a07c918abb
saikrishna-malepati <sai192sai195@gmail.com>
2024-01-29 21:22:53 +0530

    serving html,css,js(views)

--- TASK 6/app.js
+++ TASK 6/app.js
@@ -1,3 +1,4 @@
+const path =require('path');
 const express = require('express');
 const bodyParser=require ('body-parser')
 
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next)=>{
-   res.status(404).send('<h1>page not found</h1>')
+   res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 app.listen(3001);
